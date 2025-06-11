25580a3607439d935365d3ed076c46980a3fdab3
Fábio Filho <135043500+fabiodfilho@users.noreply.github.com>
2025-06-11 14:50:04 -0300

    Update vite.config.js

--- to-do-list-front/vite.config.js
+++ to-do-list-front/vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Permite acesso via IP público e rede local
-    port: 3000,      // Porta do frontend (altere se necessário)
-    strictPort: true, // Não tenta outras portas automaticamente
+    host: '0.0.0.0', // Habilita acesso externo
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      clientPort: 3000, // Importante para evitar erros de WebSocket no EC2
+    },
   },
 });
